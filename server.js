6bdab1f17ad7e9fbc2ded34c9b6d446b4d28509c
Marie <mcari1311@gmail.com>
2022-07-27 15:07:45 -0400

    takeOneDown starter outline

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     res.send("Your tip is " + tip)
 })
 
+//Magic 8 Ball  
+app.get('/magic', (req, res) => {
+    let responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+    let randomNumber = Math.floor(Math.random() * 10).toString() 
+    //not sure how to add h1 tags 
+    let randomResponse = responses[randomNumber]
+    res.send(randomResponse)
+})
+
+
 app.listen(3000, function() {
     console.log('Listening on port', port)
 })
