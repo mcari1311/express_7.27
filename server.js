47d11772ff13d2772b12b3e249e5caef27253704
Marie <mcari1311@gmail.com>
2022-07-27 13:18:27 -0400

    Greeting express application created.

--- server.js
+++ server.js
@@ -3,10 +3,15 @@ require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 3003
 
-const hello = ['hi']
-app.get('/', (req, res) => {
-    res.send(hello);
+//GREETINGS 
+app.get('/greeting', (req, res) => {
+    res.send('Hello, Stranger');
 });
+app.get('/greeting/:name', (req, res) => {
+    res.send("Hello" + req.params.name)
+});
+
+
 
 app.listen(3000, function() {
     console.log('Listening on port', port)
