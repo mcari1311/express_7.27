aa087a9bdf1e156646f5cb3719adeb78befc9446
Marie <mcari1311@gmail.com>
2022-07-28 13:15:30 -0400

    Magic 8 express application created.

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
 })
 
 //Magic 8 Ball  
-app.get('/magic', (req, res) => {
+app.get('/magic/:anything', (req, res) => {
     let responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
-    let randomNumber = Math.floor(Math.random() * 10).toString() 
+    let randomNumber = Math.floor(Math.random() * responses.length) 
     //not sure how to add h1 tags 
     let randomResponse = responses[randomNumber]
-    res.send(randomResponse)
+    res.send(req.params.anything + " : "+ randomResponse)
 })
 
 
